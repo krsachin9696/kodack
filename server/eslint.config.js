7103f649d859a5aa05e8da852054d7be5cf92517
krsachin9696 <krsachin9696@gmail.com>
2024-09-28 16:53:16 +0530

    husky setup:

---
+++ server/eslint.config.js
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,  // Keep browser globals
+        ...globals.node,     // Add Node.js globals
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      // Example rules for Node.js
+      'no-console': 'warn',  // Warn on console usage
+      'callback-return': 'error', // Enforce return of callback functions
+      // Add more Node.js specific rules here
+    },
+  },
+];
