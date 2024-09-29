61c4de620f9182a9d51ac1bc8ee8a300299daee3
krsachin9696 <krsachin9696@gmail.com>
2024-09-29 22:34:29 +0530

    material ui setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: Colors.PRIMARY,
+  //   },
+  // },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
