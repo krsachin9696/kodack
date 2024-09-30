6a580e4d90b5b9db5a21ed641be662601b606ba8
krsachin9696 <krsachin9696@gmail.com>
2024-10-01 01:27:12 +0530

    redux setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material'
 // import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
 
 const theme = createTheme({
   // palette: {
@@ -15,10 +17,12 @@ const theme = createTheme({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      {/* <BrowserRouter> */}
-        <App />
-      {/* </BrowserRouter> */}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* <BrowserRouter> */}
+          <App />
+        {/* </BrowserRouter> */}
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )
---
+++ client/src/store/authSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface ServiceState {
+  isAuthenticated: boolean
+  user: null 
+}
+
+interface AuthServicesState {
+  auth: ServiceState
+}
+
+const initialState: AuthServicesState = {
+  auth: { isAuthenticated: false, user: null },
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialState,
+  reducers: {
+    setAuthState: (state, action) => {
+      state.auth.isAuthenticated = action.payload
+    },
+    authLogin: (state, action) => {
+      state.auth.isAuthenticated = true
+      state.auth.user = action.payload
+    },
+    authLogout: (state) => {
+      state.auth.isAuthenticated = false
+      state.auth.user = null
+    },
+    setAuthUser: (state, action) => {
+      state.auth.user = action.payload
+    },
+  },
+})
+
+export const { authLogin, authLogout, setAuthState, setAuthUser } =
+  authSlice.actions
+export default authSlice.reducer
