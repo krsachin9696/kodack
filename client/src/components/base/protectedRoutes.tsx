d43fba2c5d6d61ddf76b74aa17e71696265f0b75
krsachin9696 <krsachin9696@gmail.com>
2024-10-14 12:42:24 +0530

    navigation bar

--- client/src/components/base/protectedRoutes.tsx
+++ client/src/components/base/protectedRoutes.tsx
@@ -1,13 +1,22 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
+import Navbar from '../shared/navbar';
 
 const ProtectedRoute = () => {
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated,
   );
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated ? (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  ) : (
+    <Navigate to="/login" />
+  );
+
 };
 
 export default ProtectedRoute;
