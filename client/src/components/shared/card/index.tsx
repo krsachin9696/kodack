8b420cefb294fa6184e6884f938169e752a493d8
krsachin9696 <krsachin9696@gmail.com>
2024-10-21 14:47:55 +0530

    update

--- client/src/components/shared/card/index.tsx
+++ client/src/components/shared/card/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Paper } from '@mui/material';
 
 interface CardWrapperProps {
   children: React.ReactNode;
@@ -7,7 +7,9 @@ interface CardWrapperProps {
 
 const CardWrapper: React.FC<CardWrapperProps> = ({ children, sx }) => {
   return (
-    <Box
+    <Paper
+      variant='elevation'
+      elevation={8}
       sx={{
         p: 2,
         flex: 1,
@@ -19,12 +21,12 @@ const CardWrapper: React.FC<CardWrapperProps> = ({ children, sx }) => {
         flexDirection: 'column',
         justifyContent: 'center',
         backgroundColor: 'rgba(255, 255, 255, 0.06)',
-        border: '1px solid rgba(255, 255, 255, 0.1)',
+        // border: '1px solid rgba(255, 255, 255, 0.1)',
         ...sx, // Merge additional styles
       }}
     >
       {children}
-    </Box>
+    </Paper>
   );
 };
 
--- client/src/pages/dashboard/components/LeetcodeInfo.tsx
+++ client/src/pages/dashboard/components/LeetcodeInfo.tsx
@@ -2,6 +2,7 @@ import { Box, Typography, Grid2, Avatar, Skeleton } from '@mui/material';
 import ProblemSolvedChart from './Chart';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import CardWrapper from '../../../components/shared/card';
 
 interface Badge {
   id: string;
@@ -21,6 +22,8 @@ export interface SolvedData {
   mediumSolved: number;
   hardSolved: number;
 }
+
+type SolvedKeys = keyof SolvedData;
 
 interface LeetcodeData {
   solved: SolvedData;
@@ -99,144 +102,6 @@ const LeetcodeInfo = ({ username }: { username: string }) => {
 
   if (isLoading || isError)
     return (
-      <>
-        <Grid2
-          container
-          height="100%"
-          spacing={{ xs: 1, md: 1 }}
-          columns={{ xs: 1, sm: 8, md: 12, lg: 12 }}
-          sx={{ display: 'flex' }}
-        >
-          {/* Chart Section */}
-          <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 5 }}>
-            <Box
-              height="100%"
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                borderRadius: '5px',
-                border: '1px solid rgba(255, 255, 255, 0.1)',
-                color: 'white',
-                justifyContent: 'space-around',
-              }}
-            >
-              <Skeleton
-                variant="circular"
-                width={100}
-                height={100}
-                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-              />
-              <Box
-                // width='100%'
-                gap={2}
-                sx={{
-                  mb: 2,
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  color: 'white',
-                }}
-              >
-                <Skeleton
-                  variant="rounded"
-                  width={100}
-                  height={50}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-                <Skeleton
-                  variant="rounded"
-                  width={100}
-                  height={50}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-                <Skeleton
-                  variant="rounded"
-                  width={100}
-                  height={50}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-              </Box>
-            </Box>
-          </Grid2>
-
-          {/* Badges Section */}
-          <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 7 }}>
-            <Box
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                borderRadius: '5px',
-                border: '1px solid rgba(255, 255, 255, 0.1)',
-                color: 'white',
-                justifyContent: 'center',
-                height: '100%',
-              }}
-            >
-              <div className="flex items-start flex-col w-full">
-                <Skeleton
-                  variant="text"
-                  sx={{
-                    fontSize: '1rem',
-                    backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                  }}
-                  width="100%"
-                />
-              </div>
-
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  gap: 2,
-                  height: '100%',
-                }}
-              >
-                <Skeleton
-                  variant="circular"
-                  width={100}
-                  height={100}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-                <Skeleton
-                  variant="circular"
-                  width={100}
-                  height={100}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-                <Skeleton
-                  variant="circular"
-                  width={100}
-                  height={100}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-              </Box>
-              <div className="flex items-start flex-col w-full">
-                <Skeleton
-                  variant="rounded"
-                  width="100%"
-                  height={50}
-                  sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
-                />
-              </div>
-            </Box>
-          </Grid2>
-        </Grid2>
-      </>
-    );
-  if (isError) return <Typography>Error fetching Leetcode data</Typography>;
-
-  console.log(data, 'this is data');
-  if (!data) return <Typography>No data available</Typography>;
-  const { solved, badges } = data;
-
-  return (
-    <>
       <Grid2
         container
         height="100%"
@@ -246,22 +111,14 @@ const LeetcodeInfo = ({ username }: { username: string }) => {
       >
         {/* Chart Section */}
         <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 5 }}>
-          <Box
-            height="100%"
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: 'rgba(255, 255, 255, 0.06)',
-              borderRadius: '5px',
-              border: '1px solid rgba(255, 255, 255, 0.1)',
-              color: 'white',
-              justifyContent: 'center',
-            }}
-          >
-            <ProblemSolvedChart solved={solved} />
+          <CardWrapper>
+            <Skeleton
+              variant="circular"
+              width={100}
+              height={100}
+              sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+            />
             <Box
-              // width='100%'
               gap={2}
               sx={{
                 mb: 2,
@@ -271,95 +128,41 @@ const LeetcodeInfo = ({ username }: { username: string }) => {
                 color: 'white',
               }}
             >
-              <Box
-                sx={{
-                  p: 0.2,
-                  pl: 2,
-                  pr: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                  borderRadius: '5px',
-                  border: '1px solid rgba(255, 255, 255, 0.1)',
-                  color: 'white',
-                }}
-              >
-                <div className="text-label-3 text-sm text-green-400 font-bold">
-                  Easy
-                </div>
-                <div className="text-label-3 text-sm">{solved.easySolved}</div>
-              </Box>
-              <Box
-                sx={{
-                  p: 0.2,
-                  pl: 2,
-                  pr: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                  borderRadius: '5px',
-                  border: '1px solid rgba(255, 255, 255, 0.1)',
-                  color: 'white',
-                }}
-              >
-                <div className="text-label-3 text-sm text-yellow-600 font-bold">
-                  Med.
-                </div>
-                <div className="text-label-3 text-sm">
-                  {solved.mediumSolved}
-                </div>
-              </Box>
-              <Box
-                sx={{
-                  p: 0.2,
-                  pl: 2,
-                  pr: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  backgroundColor: 'rgba(255, 255, 255, 0.06)',
-                  borderRadius: '5px',
-                  border: '1px solid rgba(255, 255, 255, 0.1)',
-                  color: 'white',
-                }}
-              >
-                <div className="text-label-3 text-sm text-red-500 font-bold">
-                  Hard
-                </div>
-                <div className="text-label-3 text-sm">{solved.hardSolved}</div>
-              </Box>
+              <Skeleton
+                variant="rounded"
+                width={100}
+                height={50}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
+              <Skeleton
+                variant="rounded"
+                width={100}
+                height={50}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
+              <Skeleton
+                variant="rounded"
+                width={100}
+                height={50}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
             </Box>
-          </Box>
+          </CardWrapper>
         </Grid2>
 
         {/* Badges Section */}
         <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 7 }}>
-          <Box
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              backgroundColor: 'rgba(255, 255, 255, 0.06)',
-              borderRadius: '5px',
-              border: '1px solid rgba(255, 255, 255, 0.1)',
-              color: 'white',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
+          <CardWrapper>
             <div className="flex items-start flex-col w-full">
-              <div className="text-label-3 text-xs text-gray-300">Badges</div>
-              <Typography variant="h6" sx={{ marginTop: '0px' }}>
-                ({badges.badgesCount})
-              </Typography>
+              <Skeleton
+                variant="text"
+                sx={{
+                  fontSize: '1rem',
+                  backgroundColor: 'rgba(255, 255, 255, 0.06)',
+                }}
+                width="100%"
+              />
             </div>
-
             <Box
               sx={{
                 display: 'flex',
@@ -369,39 +172,144 @@ const LeetcodeInfo = ({ username }: { username: string }) => {
                 height: '100%',
               }}
             >
-              {badges.badges.map((badge: Badge, index: number) => (
-                <Box
-                  key={badge.id}
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Avatar
-                    src={badge.icon}
-                    alt="B"
-                    sx={{
-                      mb: 2,
-                      width: index === 1 ? 95 : 70,
-                      height: index === 1 ? 95 : 70,
-                    }}
-                  />
-                </Box>
-              ))}
+              <Skeleton
+                variant="circular"
+                width={100}
+                height={100}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
+              <Skeleton
+                variant="circular"
+                width={100}
+                height={100}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
+              <Skeleton
+                variant="circular"
+                width={100}
+                height={100}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
             </Box>
             <div className="flex items-start flex-col w-full">
-              <div className="text-label-3 text-xs text-gray-300">
-                Most Recent Badge
-              </div>
-              <Typography variant="h6" sx={{ marginTop: '0px' }}>
-                ({badges.badges[0].displayName})
-              </Typography>
+              <Skeleton
+                variant="rounded"
+                width="100%"
+                height={50}
+                sx={{ backgroundColor: 'rgba(255, 255, 255, 0.06)' }}
+              />
             </div>
-          </Box>
+          </CardWrapper>
         </Grid2>
       </Grid2>
-    </>
+    );
+
+  if (isError) return <Typography>Error fetching Leetcode data</Typography>;
+  if (!data) return <Typography>No data available</Typography>;
+
+  const { solved, badges } = data;
+  const solvedKeys: SolvedKeys[] = ['easySolved', 'mediumSolved', 'hardSolved'];
+
+  return (
+    <Grid2
+      container
+      height="100%"
+      spacing={{ xs: 1, md: 1 }}
+      columns={{ xs: 1, sm: 8, md: 12, lg: 12 }}
+      sx={{ display: 'flex' }}
+    >
+      {/* Chart Section */}
+      <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 5 }}>
+        <CardWrapper>
+          <ProblemSolvedChart solved={solved} />
+          <Box
+            gap={2}
+            sx={{
+              mb: 2,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              color: 'white',
+            }}
+          >
+            {solvedKeys.map((key) => (
+              <Box
+                key={key}
+                sx={{
+                  p: 0.2,
+                  pl: 2,
+                  pr: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(255, 255, 255, 0.06)',
+                  borderRadius: '5px',
+                }}
+              >
+                <div
+                  className={`text-label-3 text-sm ${key === 'easySolved' ? 'text-green-400' : key === 'mediumSolved' ? 'text-yellow-600' : 'text-red-500'} font-bold`}
+                >
+                  {key.charAt(0).toUpperCase() +
+                    key.slice(1).replace('Solved', '')}
+                </div>
+                <div className="text-label-3 text-sm">{solved[key]}</div>
+              </Box>
+            ))}
+          </Box>
+        </CardWrapper>
+      </Grid2>
+
+      {/* Badges Section */}
+      <Grid2 size={{ xs: 1, sm: 8, md: 12, lg: 7 }}>
+        <CardWrapper>
+          <div className="flex items-start flex-col w-full">
+            <div className="text-label-3 text-xs text-gray-300">Badges</div>
+            <Typography variant="h6" sx={{ marginTop: '0px' }}>
+              ({badges.badgesCount})
+            </Typography>
+          </div>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 2,
+              height: '100%',
+            }}
+          >
+            {badges.badges.map((badge: Badge, index: number) => (
+              <Box
+                key={badge.id}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar
+                  src={badge.icon}
+                  alt="B"
+                  sx={{
+                    mb: 2,
+                    width: index === 1 ? 95 : 70, 
+                    height: index === 1 ? 95 : 70,
+                  }}
+                />
+              </Box>
+            ))}
+          </Box>
+          <div className="flex items-start flex-col w-full">
+            <div className="text-label-3 text-xs text-gray-300">
+              Most Recent Badge
+            </div>
+            <Typography variant="h6" sx={{ marginTop: '0px' }}>
+              ({badges.badges[0].displayName})
+            </Typography>
+          </div>
+        </CardWrapper>
+      </Grid2>
+    </Grid2>
   );
 };
 
