e7175d39ba2ee63bcfe25946ec3291135302e359
krsachin9696 <krsachin9696@gmail.com>
2024-12-10 15:00:47 +0530

    audit logs in question section

--- server/src/features/questions/questionServices.js
+++ server/src/features/questions/questionServices.js
@@ -1,6 +1,7 @@
 import * as questionRepository from './questionRepository.js';
 import * as listRepository from '../list/listRepository.js';
 import { ApiError } from '../../utils/apiError.js';
+import { logAuditTrail } from '../../utils/auditLogger.js';
 
 export const createQuestion = async (data) => {
   return await questionRepository.createQuestion(data);
@@ -11,13 +12,14 @@ export const addQuestionToListService = async (userID, listID, title, link) => {
   const existingQuestion = await questionRepository.findQuestionByLink(link);
 
   let questionID;
+  let newQuestion;
 
   if (existingQuestion) {
     // 2. If the question exists, use the existing question's ID
     questionID = existingQuestion.questionID;
   } else {
     // 3. If the question doesn't exist, create a new question
-    const newQuestion = await questionRepository.createNewQuestion(
+    newQuestion = await questionRepository.createNewQuestion(
       title,
       link,
       userID,
@@ -27,6 +29,14 @@ export const addQuestionToListService = async (userID, listID, title, link) => {
 
   // 4. Connect the question to the list using the ListQuestion table
   await listRepository.createListQuestion(listID, questionID);
+
+  logAuditTrail({
+    actorID: userID,
+    action: 'Create',
+    tableName: 'Questions',
+    recordId: questionID,
+    newData: existingQuestion ? existingQuestion : newQuestion,
+  });
 
   return {
     message: 'Question successfully added to the list.',
@@ -66,5 +76,18 @@ export const deleteQuestion = async (userID, listID, questionID) => {
     throw new ApiError(403, 'You do not have access to delete this question');
   }
 
-  return await questionRepository.deleteQuestion(listID, questionID);
+  const deletedQuestion = await questionRepository.deleteQuestion(
+    listID,
+    questionID,
+  );
+
+  logAuditTrail({
+    actorID: userID,
+    action: 'Delete',
+    tableName: 'Questions',
+    recordId: questionID,
+    newData: deletedQuestion,
+  });
+
+  return deletedQuestion;
 };
